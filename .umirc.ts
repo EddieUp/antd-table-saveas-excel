c6b317233e17a92e8ad115d8a78579f9da896f03
hongganglin <hongganglin@bianfeng.com>
2020-08-14 10:22:21 +0800

    docs: 更新outputPath

--- .umirc.ts
+++ .umirc.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   description: '基于antd table的导出插件',
   favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  outputPath: 'docs-dist',
+  outputPath: '/antd-table-saveas-excel/docs-dist',
   hash: true,
   styles: [
     'https://asset-imeete.oss-cn-hangzhou.aliyuncs.com//50048.fvpkfnangd/an/202007/image/antd.min.css',
