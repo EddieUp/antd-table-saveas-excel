aa12fd0e931023c3c9e089958d99ba071d6f6610
EddieUp <495405986@qq.com>
2020-09-07 10:38:15 +0800

    fix: 当render多个子节点的时候报错

--- src/utils/columnRender.ts
+++ src/utils/columnRender.ts
@@ -6,8 +6,8 @@ function getColumnRenderValue(data: any) {
   let o = {
     children: '',
     colSpan: 0,
-    rowSpan: 0
-  }
+    rowSpan: 0,
+  };
   if (typeof data !== 'object') {
     o.children = data;
     return o;
@@ -28,18 +28,23 @@ function getColumnRenderValue(data: any) {
     o.colSpan = data.props?.colSpan || 0;
     o.rowSpan = data.props?.rowSpan || 0;
     if (o.colSpan > 1) {
-      o.colSpan -= 1
+      o.colSpan -= 1;
     }
     if (o.rowSpan > 1) {
-      o.rowSpan -= 1
+      o.rowSpan -= 1;
     }
   }
   return o;
 }
 
 function getLastChildren(data: any): string {
+  if (Array.isArray(data)) {
+    for (let item of data) {
+      return getLastChildren(item);
+    }
+  }
   const props = data.props;
-  if (props.children && typeof props.children === 'object') {
+  if (props?.children && typeof props.children === 'object') {
     return getLastChildren(props.children);
   }
   return props?.children;
