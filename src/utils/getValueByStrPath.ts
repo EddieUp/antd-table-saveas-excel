22732adb5f0ac273eab36b1151c5222760404edb
EddieUp <495405986@qq.com>
2021-07-02 17:16:14 +0800

    fix: 当dataIndex为number时显示值异常

--- src/utils/getValueByStrPath.ts
+++ src/utils/getValueByStrPath.ts
@@ -1,6 +1,6 @@
 
 function getValueByStrPath(data: Record<string, any>, keys: string | string[]) {
-  if (typeof keys === 'string') return data[keys];
+  if (typeof keys === 'string' || typeof keys === 'number') return data[keys];
   let index = 0;
   let key = keys[0];
   let value = data;
