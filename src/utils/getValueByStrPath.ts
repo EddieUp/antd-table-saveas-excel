8bc43b4c071c46a6ce5980d6164e412abb7fd070
EddieUp <495405986@qq.com>
2021-07-27 16:24:10 +0800

    fix: 添加dataIndex路径查找数据时的错误处理

--- src/utils/getValueByStrPath.ts
+++ src/utils/getValueByStrPath.ts
@@ -1,15 +1,27 @@
 
 function getValueByStrPath(data: Record<string, any>, keys: string | string[]) {
-  if (typeof keys === 'string' || typeof keys === 'number') return data[keys];
+  if (typeof keys !== 'object') return data[keys];
+  if (!Array.isArray(keys)) {
+    console.error('excel-dataIndex: Expected string or string[]')
+    return '';
+  }
   let index = 0;
   let key = keys[0];
   let value = data;
-  while (key) {
-    value = value[key]
-    index++;
-    key = keys[index];
+  try {
+    while (key) {
+      value = value[key]
+      index++;
+      key = keys[index];
+    }
+  } catch (e) {
+    return '';
   }
-  return value;
+  if (typeof value !== 'object') {
+    return value;
+  }
+  console.error('excel-cell-value: Unexpected object')
+  return '';
 }
 
 export default getValueByStrPath;
